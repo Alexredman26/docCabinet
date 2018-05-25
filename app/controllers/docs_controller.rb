f7a0d14fec29f1b52424a14e4ae9d7b12fc55cd6
Alex Redman <alexredman26@gmail.com>
2018-05-25 18:03:35 +0000

    added show doc functionality

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,8 +1,12 @@
 class DocsController < ApplicationController
+    before_action :find_doc, only: [:show, :edit, :update, :destroy]
+   
     def index
+        
     end
     
     def show
+        
     end
     
     def new
@@ -30,6 +34,7 @@ class DocsController < ApplicationController
     
     private
         def find_doc
+            @doc = Doc.find(params[:id])
         end
         
         def doc_params
